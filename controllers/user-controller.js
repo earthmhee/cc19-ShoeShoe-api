8f59104fc46d4e4b8e936d03ac2e52cf0bb2fd57
SuphakornLVK <SuphakornLVK@gmail.com>
2025-03-11 16:09:50 +0700

    setup/user auth

---
+++ controllers/user-controller.js
@@ -0,0 +1,24 @@
+const createError = require('../utils/createError')
+
+exports.getMyAccount = async (req, res, next) => {
+    try {
+        const { id } = req.user
+        console.log('User Id : ', id);
+        // look for user
+        const rs = await prisma.user.findFirst({ where: { clerkID: id}})
+        res.status(200).json({ msg: "My account get", rs})
+    } catch (error) {
+        next (error)
+    }
+}
+
+// dummy for now
+exports.createUpdateAccount = async (req, res, next) => {
+    try {
+        console.log('Dummy for update');
+        const rs = await 'Updated'
+        res.status(200).json({ msg: "Create Update", rs})
+    } catch (error) {
+        next (error)
+    }
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const morgan = require("morgan");
+const helmet = require('helmet')
+const express = require("express");
+const cors = require("cors");
+const notFound = require("./middlewares/notFound");
+const handleError = require("./middlewares/handleError");
+const app = express()
+
+// import routes ...
+const userRoute = require('./routes/user-route')
+
+// import Middlewares ...
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms")); // check logging request
+app.use(helmet()) // security for app
+app.use(cors()); // allows connection
+
+// route connect ...
+app.use(express.json());
+
+app.use("/user", userRoute)
+
+// notFound - send 404
+app.use( notFound );
+
+// error check - send err code or 500
+app.use( handleError );
+
+// run server on port
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("Server is running on PORT", port));
