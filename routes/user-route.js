0b263063ef28ceed83ab51c356e46638a07ddc8c
NanYodrabum <nanyodrabum@gmail.com>
2025-03-18 14:22:18 +0700

    adminroute

--- routes/user-route.js
+++ routes/user-route.js
@@ -3,6 +3,8 @@ const {
 	createUpdateAccount,
 	createNewAccount,
 	changePassword,
+	getUsers,
+	getUserById,
 } = require("../controllers/user-controller");
 const authenticate = require("../middlewares/authenticate");
 const userRoute = express.Router();
@@ -10,5 +12,9 @@ const userRoute = express.Router();
 userRoute.get("/my-account", authenticate, createNewAccount);
 userRoute.put("/update-account", authenticate, createUpdateAccount);
 userRoute.put("/change-password", authenticate, changePassword);
+
+// User data retrieval routes
+userRoute.get("/all", authenticate, getUsers);
+userRoute.get("/:id", authenticate, getUserById);
 
 module.exports = userRoute;
