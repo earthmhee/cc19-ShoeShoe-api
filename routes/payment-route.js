c2dad001a4b536d50b8e69c478cc0c9429d16d26
Chattaphoom Kasisomang <chattaphoom.k@gmail.com>
2025-03-18 17:37:41 +0700

    edit payment-route

--- routes/payment-route.js
+++ routes/payment-route.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { checkout, checkOutStatus } = require("../controllers/payment-controller");
+const {
+	checkout,
+	checkOutStatus,
+} = require("../controllers/payment-controller");
 const paymentRoute = express.Router();
 
-paymentRoute.post("/checkout", checkout);
+paymentRoute.post("/checkout/:id", checkout);
 paymentRoute.post("/checkout-status/:session_id", checkOutStatus);
 
 module.exports = paymentRoute;
